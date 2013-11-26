8bf17a058c2bdca577502675c37629c9dbef595f
Phong Phạm <s.n.e.l2810@gmail.com>
2013-11-26 19:01:33 +0700

    Exhibition
    asdasda

--- InstutiteOfFineArt/Daos/CompetitionDAO.cs
+++ InstutiteOfFineArt/Daos/CompetitionDAO.cs
@@ -12,7 +12,7 @@ namespace InstutiteOfFineArt.Daos
 {
     public class CompetitionDAO
     {
-        public DataTable All()
+        public static DataTable All()
         {
 
             DBUtilities.objConnection = new SqlConnection(DBUtilities.connStr);
@@ -23,7 +23,7 @@ namespace InstutiteOfFineArt.Daos
             return dt;
         }
 
-        public Competition Find(int id)
+        public static Competition Find(int id)
         {
             DBUtilities.objConnection = new SqlConnection(DBUtilities.connStr);
             DataTable dt = new DataTable();
@@ -46,7 +46,7 @@ namespace InstutiteOfFineArt.Daos
             return null;
         }
 
-        public DataTable Where(Dictionary<string, object> query)
+        public static DataTable Where(Dictionary<string, object> query)
         {
             DBUtilities.objConnection = new SqlConnection(DBUtilities.connStr);
             DataTable dt = new DataTable();
@@ -70,7 +70,7 @@ namespace InstutiteOfFineArt.Daos
             adap.Fill(dt);
             return dt;
         }
-        public DataTable Search(List<string> query)
+        public static DataTable Search(List<string> query)
         {
             DBUtilities.objConnection = new SqlConnection(DBUtilities.connStr);
             DataTable dt = new DataTable();
@@ -90,7 +90,7 @@ namespace InstutiteOfFineArt.Daos
         }
 
 
-        public bool Create(Competition c)
+        public static bool Create(Competition c)
         {
             DBUtilities.Connection();
             try
@@ -120,7 +120,7 @@ namespace InstutiteOfFineArt.Daos
             }
         }
 
-        public bool Update(Competition c)
+        public static bool Update(Competition c)
         {
             DBUtilities.Connection();
             try
@@ -176,7 +176,7 @@ namespace InstutiteOfFineArt.Daos
             }
         }
 
-        public bool Destroy(Competition c)
+        public static bool Destroy(Competition c)
         {
             DBUtilities.Connection();
             try
