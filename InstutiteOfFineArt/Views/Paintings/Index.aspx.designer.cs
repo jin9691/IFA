efd7a38d9fa09bf4d6cb814090a16613708c2f91
jin9691 <jin9691@gmail.com>
2013-11-28 03:00:26 +0700

    Database + Painting Upload

--- InstutiteOfFineArt/Views/Paintings/Index.aspx.designer.cs
+++ InstutiteOfFineArt/Views/Paintings/Index.aspx.designer.cs
@@ -29,14 +29,5 @@ namespace InstutiteOfFineArt.Views.Paintings {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm Form1;
-        
-        /// <summary>
-        /// gdvUsers control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView gdvUsers;
     }
 }
