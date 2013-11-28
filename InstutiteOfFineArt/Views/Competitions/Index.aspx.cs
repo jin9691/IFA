4222b7f45b15e63a9529a1854be659f338a8f511
Phong Phạm <s.n.e.l2810@gmail.com>
2013-11-28 18:56:50 +0700

    Competition + fix DAO

--- InstutiteOfFineArt/Views/Competitions/Index.aspx.cs
+++ InstutiteOfFineArt/Views/Competitions/Index.aspx.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using InstutiteOfFineArt.Codes;
+using InstutiteOfFineArt.Daos;
+using InstutiteOfFineArt.Models;
 
 namespace InstutiteOfFineArt.Views.Competitions
 {
@@ -11,7 +15,64 @@ namespace InstutiteOfFineArt.Views.Competitions
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                Load_Data();
+            }
+        }
+        private void Load_Data()
+        {
+            DataTable dtCompetition = CompetitionDAO.All();
+            gdvCompetitions.DataSource = dtCompetition;
+            gdvCompetitions.DataBind();
+            lbCountCompetition.Text = dtCompetition.Rows.Count.ToString();
+        }
 
+        public string Staff_Label(object id)
+        {
+            int val = Convert.ToInt32(id.ToString());
+            User user = InstutiteOfFineArt.Daos.UserDAO.Find(val);
+            return user.Name;
+        }
+        public string Description_Label(object des)
+        {
+            string val = Convert.ToString(des);
+            if (val.Length > 10)
+                val = val.Substring(0, 10) + "...";
+            return val;
+
+        }
+        protected void Destroy_Click(object sender, EventArgs e)
+        {
+            LinkButton a = sender as LinkButton;
+            int id = Convert.ToInt32(a.CommandArgument);
+            Dictionary<string, object> query1 = new Dictionary<string, object>();
+                query1.Add("CompetitionId", id);
+             Competition u = CompetitionDAO.Find(id);
+                if (PaintingDAO.Where(query1).Rows.Count >= 1 || AwardDAO.Where(query1).Rows.Count >= 1)
+                {
+                    Flash.dictFlash.Add("danger", String.Format("Cannot delete competition [<b>{0}</b>].Please delete all the painting/ award related.",u.Topic ));
+                  
+                }
+                else {
+                   
+                    if (CompetitionDAO.Destroy(u))
+                    {
+                        Flash.dictFlash.Add("success", String.Format("Delete competition [<b>{0}</b>] successfully", u.Topic));
+                    }
+                    else
+                    {
+                        Flash.dictFlash.Add("success", String.Format("Cannot delete competition [<b>{0}</b>]", u.Topic));
+                    }  
+                }
+            
+            Response.Redirect("Index.aspx");
+        }
+
+        protected void gdvCompetitions_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gdvCompetitions.PageIndex = e.NewPageIndex;
+            Load_Data();
         }
     }
 }
--- InstutiteOfFineArt/Views/Competitions/New.aspx.cs
+++ InstutiteOfFineArt/Views/Competitions/New.aspx.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using InstutiteOfFineArt.Codes;
+using InstutiteOfFineArt.Daos;
+using InstutiteOfFineArt.Models;
 
 namespace InstutiteOfFineArt.Views.Competitions
 {
@@ -11,7 +15,92 @@ namespace InstutiteOfFineArt.Views.Competitions
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+         
+            if (Session["current_user"] != null)
+                if (UserDAO.Find(Int32.Parse(Session["current_user"].ToString())).Permission == 3)
+            {
+                if (!IsPostBack)
+                {
+                    Dictionary<string, object> query = new Dictionary<string, object>();
+                    query.Add("Permission", 1);
+                    DataTable dtStudent = UserDAO.Where(query);
+                    cbStaff.DataValueField = "Id";
+                    cbStaff.DataTextField = "Name";
+                    cbStaff.DataSource = dtStudent;
+                    cbStaff.DataBind();
+                    cbStaff.SelectedIndex = 0;
+                }
+            }
+        
+                Response.Redirect("Index.aspx");
+          
 
+          
+        }
+        protected void btnAccept_Click(object sender, EventArgs e)
+        {
+            if (validateControl())
+            {
+                
+                Competition c = new Competition();
+                c.Remark = txtRemark.Text;
+                if (cbStaff.SelectedValue != null)
+                    c.StaffId = Convert.ToInt32(cbStaff.SelectedValue);
+                c.StartDate = DateTime.Parse(txtStartDate.Text);
+                c.DueDate = DateTime.Parse(txtEndDate.Text);
+                c.Topic = txtTopic.Text;
+                c.Condition = txtConditition.Text;
+                c.CompetitionDescription = txtConditition.Text;
+                if (CompetitionDAO.Create(c))
+                {
+                    Flash.dictFlash.Add("success", String.Format("Created competition [<b>{0}</b>] successfully", c.Topic));
+                    Response.Redirect("Index.aspx");
+                }
+                else
+                {
+                    Flash.dictFlash.Add("danger", " Cannot create competition !!!");
+                    Response.Redirect("New.aspx");
+                }
+            }
+        }
+        private bool validateControl()
+        {
+            if (!ValidateClass.Validate_Require(txtTopic.Text))
+            {
+                lbTopicErr.Text = "* Topic cannot be blank";
+                return false;
+            }
+            else lbTopicErr.Text = "";
+            if (!ValidateClass.Validate_Datetime(txtStartDate.Text))
+            {
+                lbStartDateErr.Text = "* Start date must be valid date";
+                return false;
+            }
+            else
+                lbStartDateErr.Text = "";
+            if (!ValidateClass.Validate_Datetime(txtEndDate.Text))
+            {
+                lbEndDateErr.Text = "* End date must be valid date";
+                return false;
+            }
+            else
+                lbEndDateErr.Text = "";
+            if (!ValidateClass.Validate_Require(txtConditition.Text))
+            {
+                lbCondititionErr.Text = "* Conditition can not be blank";
+                return false;
+            }
+            else
+                lbCondititionErr.Text = "";
+            if (!ValidateClass.Validate_Require(txtRemark.Text))
+            {
+                lbRemarkErr.Text = "* Remark can not be blank";
+                return false;
+            }
+            else
+                lbRemarkErr.Text = "";
+            return true;
+            
         }
     }
 }
