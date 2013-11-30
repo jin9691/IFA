297a8a9695f5ba69114f3c1654b1d32475a149bb
hieunn <nnhieu.t@gmail.com>
2013-11-30 12:19:04 +0700

    Student unique Competition

--- InstutiteOfFineArt/Views/Competitions/Show.aspx.cs
+++ InstutiteOfFineArt/Views/Competitions/Show.aspx.cs
@@ -36,31 +36,41 @@ namespace InstutiteOfFineArt.Views.Competitions
 
         protected void btnUpload_Click(object sender, EventArgs e)
         {
-            int CompetitionID = (Request.QueryString["ID"] != null) ? Convert.ToInt32(Request.QueryString["ID"]) : 1;;
-            var date = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            int CompetitionID = (Request.QueryString["ID"] != null) ? Convert.ToInt32(Request.QueryString["ID"]) : 1;
+            
             if (validateControl())
-            {
-                Painting p = new Painting();
-                p.PaintingDescription = txtDesc.Text;
-                p.PaintingURL = UploadImage(fileUploadField);
-                p.CompetitionId = CompetitionID;
-                p.UploadDate = DateTime.Now;
-                p.LastModify = DateTime.Now;
-                if (Session["current_user"] != null)
+            {              
+                User u = (User)Session["current_user"];                    
+                //Check Student unique Competition
+                Dictionary<string, object> query = new Dictionary<string, object>();
+                query.Add("CompetitionId", CompetitionID);
+                query.Add("StudentId", Convert.ToInt32(u.Id));
+                DataTable dt = PaintingDAO.Where(query);
+                if (dt.Rows.Count == 0)
                 {
-                    User u = (User)Session["current_user"];
+                    //Upload painting
+                    Painting p = new Painting();
+                    p.PaintingDescription = txtDesc.Text;
+                    p.PaintingURL = UploadImage(fileUploadField);
+                    p.CompetitionId = CompetitionID;
+                    p.UploadDate = DateTime.Now;
+                    p.LastModify = DateTime.Now;
                     p.StudentId = Convert.ToInt32(u.Id);
+                    if (PaintingDAO.CreateForStudent(p))
+                    {
+                        Flash.dictFlash.Add("success", String.Format("Upload painting [<b>{0}</b>] successfully", fileUploadField.FileName));
+                        Response.Redirect("Show.aspx");
+                    }
+                    else
+                    {
+                        Flash.dictFlash.Add("danger", " Cannot create Painting !!!");
+                        Response.Redirect("Show.aspx");
+                    }
                 }
-                if (PaintingDAO.CreateForStudent(p))
-                {
-                    Flash.dictFlash.Add("success", String.Format("Upload painting [<b>{0}</b>] successfully", fileUploadField.FileName));
-                    Response.Redirect("Show.aspx");
+                else {
+                    Flash.dictFlash.Add("warning", String.Format("You have sent picture with this competition"));
                 }
-                else
-                {
-                    Flash.dictFlash.Add("danger", " Cannot create Painting !!!");
-                    Response.Redirect("Show.aspx");
-                }
+                
             }
         }
 
