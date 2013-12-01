3b2ec60734b70c896085c067040282dfe96dda09
jin9691 <jin9691@gmail.com>
2013-12-01 22:07:35 +0700

    user role

--- InstutiteOfFineArt/Views/MasterPages/Admin.Master.cs
+++ InstutiteOfFineArt/Views/MasterPages/Admin.Master.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using InstutiteOfFineArt.Models;
 
 namespace InstutiteOfFineArt.Views.MasterPages
 {
@@ -11,6 +12,16 @@ namespace InstutiteOfFineArt.Views.MasterPages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["current_user"] != null)
+            {
+                User u = (User)Session["current_user"];
+                if (u.Permission != 0)
+                    Response.Redirect("Home.aspx");
+            }
+            else
+            {
+                Response.Redirect("../StaticPages/Home.aspx");
+            }
         }
     }
 }
